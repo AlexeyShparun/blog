0256ce0072cf24063e1dce40b885478cbabe39ab
Alexey <an.shparun@yandex.by>
2023-09-19 18:25:10 +0400

    read

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #  root "articlels#index"
-
-  get "/articles", to: "articles#index"
   root "articles#index"
+
+  resources :articles
   # Defines the root path route ("/")
   # root "articles#index"
 end
